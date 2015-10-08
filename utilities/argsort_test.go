03e72b550ca411f8494613b3e1ace75418b80521
Kirill Temlyakov <Kirill@simplerelevance.com>
2015-10-08 12:06:32 -0500

    added argsort_test.go

---
+++ utilities/argsort_test.go
@@ -0,0 +1,16 @@
+package argsort
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestArgsort(t *testing.T) {
+	my_slice := []float64 {5,6,3,2,9,1}
+	result := Argsort(my_slice)
+	expected := []int {4,1,0,2,3,5}
+
+	if reflect.DeepEqual(result, expected) == false {
+		t.Errorf("Expected %x, but got %x", expected, result)
+	}
+}
