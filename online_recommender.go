afc7c57ac48866aeb520d307a156d1886bb90bd4
Kirill Temlyakov <Kirill@simplerelevance.com>
2015-10-08 12:08:03 -0500

    removed commented out portion

--- online_recommender.go
+++ online_recommender.go
@@ -152,36 +152,3 @@ func make_user_id_to_actions() map[string][]string {
 	user_id_to_actions["emily"] = []string {"12","14",}
 	return user_id_to_actions
 }
-
-// This should be in a different file
-// type TwoSlices struct {
-// 	indices  []int
-// 	scores  []float64
-// }
-
-// type SortByOther TwoSlices
-
-// func (sbo SortByOther) Len() int {
-// 	return len(sbo.indices)
-// }
-
-// func (sbo SortByOther) Swap(i, j int) {
-// 	sbo.indices[i], sbo.indices[j] = sbo.indices[j], sbo.indices[i]
-// 	sbo.scores[i], sbo.scores[j] = sbo.scores[j], sbo.scores[i]
-// }
-
-// func (sbo SortByOther) Less(i, j int) bool {
-// 	return sbo.scores[i] > sbo.scores[j] 
-// }
-
-// func argsort(scores []float64) []int {
-// 	indices := make([]int, len(scores))
-// 	for i := 0; i < len(scores); i++ {
-// 		indices[i] = i
-// 	}
-// 	my_two_slices := TwoSlices{indices: indices, scores: scores}
-// 	sort.Sort(SortByOther(my_two_slices))
-// 	return my_two_slices.indices
-
-// }
-
